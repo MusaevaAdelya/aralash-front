3571c45c2527a51b87c9f0a8a1e11ec1be8aa567
Adelya <msvadelya@gmail.com>
2024-06-01 02:24:15 +0600

    finished reviews section

--- src/components/specific/CommentBlock.jsx
+++ src/components/specific/CommentBlock.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function CommentBlock({image, name, text, opacity}) {
   return (
-    <div className='bg-white rounded-xl flex max-w-[730px] py-7 px-9 space-x-7 text-dark-200 items-center mb-5' style={{opacity:opacity}}>
+    <div className='bg-white rounded-xl flex max-w-[730px] py-7 px-9  text-dark-200 items-center mb-5 lg:flex-row flex-col gap-7 text-center lg:text-start' style={{opacity:opacity}}>
         <img src={image} alt="avatar" className='rounded-full w-[111px] h-[111px]'/>
         <div className=''>
-            <p className='text-xl mb-5'>{`"${text}"`}</p>
+            <p className='mb-5 text-base md:text-xl'>{`"${text}"`}</p>
             <span>{name}</span>
         </div>
     </div>
--- src/components/specific/ReviewSection.jsx
+++ src/components/specific/ReviewSection.jsx
@@ -1,4 +1,6 @@
 import CommentBlock from "./CommentBlock";
+import { Link } from "react-router-dom";
+import { ArrowRightIcon } from "@heroicons/react/16/solid";
 
 function ReviewSection() {
   return (
@@ -6,11 +8,11 @@ function ReviewSection() {
       <img
         src="/images/wave-upper.svg"
         alt="wave"
-        className="w-full bg-dark-300 -mt-[326px] "
+        className="w-full bg-dark-300 lg:-mt-[326px] -mt-[200px]"
       />
-      <div className=" pt-[320px] bg-dark-300">
+      <div className=" pt-[320px] bg-dark-300 pb-12">
         <div className="mx-auto container max-w-[1335px] lg:px-0 px-5 flex flex-col justify-center items-center">
-          <h5 className="text-center text-white font-bold text-3xl mb-16">
+          <h5 className="mb-16 text-2xl font-bold text-center text-white md:text-3xl">
             +1,749 команд поделились своим опытом
             <br /> использования нашего сервиса!
           </h5>
@@ -32,6 +34,7 @@ function ReviewSection() {
             text="Обязательный инструмент для любого бизнеса, который хочет принимать платежи онлайн быстро и безопасно."
             opacity={0.5}
           />
+          <Link to="/" className="flex gap-3 mt-12 text-lg font-bold text-tertiary hover:underline">Все отзывы<ArrowRightIcon className="w-6"/></Link>
         </div>
       </div>
       <img
