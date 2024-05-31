422da7c8b5d914adfcc285065e9c79b16e32a21d
Adelya <msvadelya@gmail.com>
2024-06-01 04:19:07 +0600

    added gradient circle to the login and register pages

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -5,6 +5,7 @@ import GoogleSignUp from "../components/specific/GoogleSignUp";
 function RegisterPage() {
   return (
     <section className="w-full mx-auto lg:w-2/5 container max-w-[1335px] lg:px-0 px-5 mt-28 mb-48">
+      <img src="/images/gradient-circle.png" alt="gradient decoration" className="absolute top-0 left-0 -z-10" />
       <FormHeader
         heading="Signup to create an account"
         paragraph="Already have an account? "
