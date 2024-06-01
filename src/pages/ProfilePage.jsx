3f020bed1ac2a429d9913941dddbc69fd9e11b02
Aidar <97720321+Aidar912@users.noreply.github.com>
2024-06-01 12:38:22 +0600

    img

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -17,7 +17,7 @@ function ProfilePage() {
 
   return (
     <section className="min-h-screen  mx-auto container max-w-[1335px] lg:px-0 px-5 mt-20">
-      <ProfileHeader name={user? user.username:""} balance={user? user.balance:""}/>
+      <ProfileHeader name={user? user.username:""} balance={user? user.balance:""} avatar={user? user.photo:''}/>
       <ProfileTabs/>
     </section>
   );
